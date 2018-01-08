1b390ae5ed23d6660f8a0d30e86e7b70dcbde1be
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2018-01-08 15:53:53 -0500

    Added code to train and test. 99.2 accuracy

--- mnist_deep.py
+++ mnist_deep.py
@@ -1,3 +1,4 @@
+from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 
 def deepnn(x):
@@ -39,5 +40,40 @@ def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],\
-                                    strides=[1, 2, 2, 1], padding='SAME'))
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], \
+                                    strides=[1, 2, 2, 1], padding='SAME')
+
+def main():
+    mnist = input_data.read_data_sets("data/", one_hot=True)
+
+    x = tf.placeholder(tf.float32, [None, 784])
+    y_ = tf.placeholder(tf.float32, [None, 10])
+
+    y_conv, keep_prob = deepnn(x)
+
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_,
+                                                            logits=y_conv)
+
+    cross_entropy = tf.reduce_mean(cross_entropy)
+
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+    correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y_conv, 1))
+    correct_prediction = tf.cast(correct_prediction, tf.float32)
+
+    accuracy = tf.reduce_mean(correct_prediction)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(20000):
+            batch = mnist.train.next_batch(50)
+            if i % 100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={
+                    x: batch[0], y_: batch[1], keep_prob: 1.0})
+                print('step %d, training accuracy %g' % (i, train_accuracy))
+            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+        print('test accuracy %g' % accuracy.eval(feed_dict={
+            x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+main()
